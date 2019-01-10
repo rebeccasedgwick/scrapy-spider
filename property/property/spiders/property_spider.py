2aa24a47365e907dffec60899cc8fe2c6fac3abd
rebeccasedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2019-01-10 12:53:41 +0000

    basic text fields of property data

--- property/property/spiders/property_spider.py
+++ property/property/spiders/property_spider.py
@@ -15,8 +15,18 @@ class SVNIndividualPageSpider(scrapy.Spider):
 
     def parse_buildout_iframe(self, response):
         yield {
-            'name': response.css('.header-text h1::text').extract_first(),
+            'property_name': response.css('.header-text h1::text').extract_first(),
             'address': response.css('.text-muted::text').extract(),
+            'price': response.css('tr:nth-child(1) > td:nth-child(2)::text').extract_first(),
+            'property-type': response.css('tr:nth-child(2) > td:nth-child(2)::text').extract_first(),
+            'size': response.css('tr:nth-child(3) > td:nth-child(2)::text').extract_first(),
+            'description': response.css('.js-description-content::text').extract_first(),
+            'highlghts': {
+                'highlight_1': response.css('ul > div > div:nth-child(1) > li:nth-child(1)::text').extract_first(),
+                'highlight_2': response.css('ul > div > div:nth-child(1) > li:nth-child(2)::text').extract_first(),
+                'highlight_3': response.css('ul > div > div:nth-child(2) > li:nth-child(1)::text').extract_first(),
+                },
+            'location': response.css('.js-location-content::text').extract_first(),
         }
 
     def parse(self, response):
